191aca83c84b3db35693593b1af32731b2563eb3
dmitry.kalashnikov <dmitry-kalashnikov@yandex.ru>
2026-03-16 23:44:49 +0700

    Quartz sync: Mar 16, 2026, 11:44 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -80,6 +80,7 @@ const config: QuartzConfig = {
       Plugin.ContentPage(),
       Plugin.FolderPage(),
       Plugin.TagPage(),
+      Plugin.Sitemap(),
       Plugin.ContentIndex({
         enableSiteMap: true,
         enableRSS: true,
